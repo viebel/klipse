09825051d180cd57a1d5afe4a68ff3aaa429591a
Yehonathan Sharvit <viebel@gmail.com>
2015-11-19 16:08:41 +0200

    now it works!

--- script/figwheel.clj
+++ script/figwheel.clj
@@ -8,7 +8,7 @@
    [{:id "dev"
      :figwheel true
      :source-paths ["src"]
-     :compiler {:main 'om-tutorial.core
+     :compiler {:main 'my-cljs-compiler-in-cljs.core
                 :asset-path "js"
                 :output-to "resources/public/js/main.js"
                 :output-dir "resources/public/js"
--- src/my_cljs_compiler_in_cljs/core.cljs
+++ src/my_cljs_compiler_in_cljs/core.cljs
@@ -1,4 +1,10 @@
-(ns my-cljs-compiler-in-cljs.core)
+(ns my-cljs-compiler-in-cljs.core
+  (:require [cljs.tools.reader :as reader]))
+
+
 (enable-console-print!)
-(print "hello")
+(print "hello World")
+
+(print (reader/read-string "(set! (.. js/document -body -style -backgroundColor \"white\"))"))
+
 

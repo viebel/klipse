ae7b2fbe21e012dd57ae04cbf62a64192601264c
Yehonathan Sharvit <viebel@gmail.com>
2015-11-22 17:54:43 +0200

    anlz

--- src/my_cljs_compiler_in_cljs/core.cljs
+++ src/my_cljs_compiler_in_cljs/core.cljs
@@ -1,10 +1,26 @@
 (ns my-cljs-compiler-in-cljs.core
-  (:require [cljs.tools.reader :as reader]))
+  (:require [cljs.tools.reader :as reader]
+            [cljs.compiler :as compiler]
+            [cljs.analyzer :as analyzer]))
 
 
 (enable-console-print!)
 (print "hello World")
 
 (print (reader/read-string "(set! (.. js/document -body -style -backgroundColor \"white\"))"))
+(print (map type (reader/read-string "(set! (.. js/document -body -style -backgroundColor) \"white\")")))
+
+
+(def user-env '{:ns {:name cljs.user}
+                :locals {}})
+
+
+
+(->>  ;"(set! (.. js/document -body -style -backgroundColor \"white\"))"
+     "(cljs.core/clj->js (cljs.core/map cljs.core/inc [9 (+ 1 2) 2 3]))"
+     reader/read-string
+     (analyzer/analyze user-env)
+     compiler/emit-str
+     (println "js code: "))
 
 

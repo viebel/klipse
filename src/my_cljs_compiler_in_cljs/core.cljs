8d65699c17d32ce8c32c87d82c92be1daa8f4081
Yehonathan Sharvit <viebel@gmail.com>
2016-01-17 16:36:48 +0200

    make it work

--- src/my_cljs_compiler_in_cljs/core.cljs
+++ src/my_cljs_compiler_in_cljs/core.cljs
@@ -10,17 +10,6 @@
 ;; create cljs.user
 (set! (.. js/window -cljs -user) #js {})
 
-(defn cb [{:keys [value error]}]
-    (if-not error
-      (println value)
-      (.error js/console error)))
-
-(cljs/eval-str (cljs/empty-state) "(def a 5) (map #(inc %)  [1 2 a])" 
-                 'test {:eval cljs/js-eval} cb)
-(cljs/compile-str  (cljs/empty-state) "(def a 999) (map #(* % 9)  [1 2 a]) (cljs.core/map #(inc %) [10 20 90])" cb)
-(cljs/eval-str (cljs/empty-state) "(map #(inc %)  [10000 2]) (map #(inc %)  [1 2])" 
-                 'test {:eval cljs/js-eval} cb)
-
 (defonce state (atom {:compilation-results ""
                   :evalutation-results ""}))
 (defonce eval-res nil)
@@ -30,7 +19,7 @@
   (render [this]
           (let [{:keys [compilation-results evalutation-results-js evalutation-results-clj input]} (om/props this)]
             (dom/div nil
-                     (dom/textarea #js {:rows 10 :cols 80
+                     (dom/textarea #js {:rows 20 :cols 80
                                         :value input
                                         :onChange (fn [e]
                                                     (let [content (.. e -target -value)]
@@ -44,7 +33,7 @@
                                                                                    (.. error -cause -message)
                                                                                    value)]
                                                                          (swap! state assoc :evalutation-results-clj (str res))
-                                                                         (swap! state assoc :evalutation-results-js (str (.stringify js/JSON res nil 4))))))
+                                                                         (swap! state assoc :evalutation-results-js #_"Not yet available" (str (.stringify js/JSON res nil 4))))))
                                                       (cljs/compile-str (cljs/empty-state) content
                                                                         (fn [{:keys [value error]}]
                                                                           (if error
@@ -52,8 +41,9 @@
                                                                                       (.. error -cause -message)
                                                                                       value)]
                                                                             (swap! state assoc :compilation-results res))))))})
-                     (dom/div #js {:className "evaluation-results-clj"} evalutation-results-clj)
-                     (dom/div #js {:className "evaluation-results-js"} evalutation-results-js)
+                     (dom/section nil
+                                  (dom/div #js {:className "evaluation-results-clj"} evalutation-results-clj)
+                                  (dom/div #js {:className "evaluation-results-js"} evalutation-results-js))
                      (dom/div #js {:className "compilation-results"} compilation-results)))))
 
 
@@ -62,3 +52,4 @@
   (om/reconciler {:state state}))
 
 (om/add-root! reconciler Compiler (gdom/getElement "compiler"))
+(swap! state assoc :input "(str \"Hello World!\")")

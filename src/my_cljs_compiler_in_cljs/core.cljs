5618d4be8bb5d66835003f0f8d3de3b38faabd76
Yehonathan Sharvit <viebel@gmail.com>
2016-02-09 11:54:46 +0200

    remove unneeded files

--- src/my_cljs_compiler_in_cljs/core.cljs
+++ src/my_cljs_compiler_in_cljs/core.cljs
@@ -1,10 +1,7 @@
 (ns my-cljs-compiler-in-cljs.core
   (:require 
-    [my-cljs-compiler-in-cljs.config-logic]
-    [my-cljs-compiler-in-cljs.errors]
     [my-cljs-compiler-in-cljs.compiler]
-    [my-cljs-compiler-in-cljs.play]
-    ))
+    [my-cljs-compiler-in-cljs.play]))
 
 (enable-console-print!)
 (print "T BB bcore")

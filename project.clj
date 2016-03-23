0ca751f1225e250aeb2334329c79bb901c683be7
Yehonathan Sharvit <viebel@gmail.com>
2016-03-23 10:05:35 +0200

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klipse "0.1.0"
+(defproject klipse "0.2.0"
   :description "Cljs compiler in cljs"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.8.34"]

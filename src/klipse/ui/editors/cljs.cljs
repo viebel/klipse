a9cc8605526e2f3b72f018cc21b0bb263ee1cb0b
Yehonathan Sharvit <viebel@gmail.com>
2016-06-10 15:20:12 +0300

    fix first init bug

--- src/klipse/ui/editors/cljs.cljs
+++ src/klipse/ui/editors/cljs.cljs
@@ -22,20 +22,20 @@
 (defn process-input [component s]
   (when-not (blank? s)
     (om/transact! component 
-                  [(list 'input/save     {:value s})
-                   (list 'cljs/compile   {:value s})
-                   (list 'js/eval        {:value s})
-                   (list 'clj/eval       {:value s})])))
+                  [`(input/save   {:value ~s})
+                   `(cljs/compile {:value ~s})
+                   `(js/eval      {:value ~s})
+                   `(clj/eval     {:value ~s})])))
 
 (defn init-input [component s]
   (om/transact! component
                   [(list 'input/save     {:value s})]))
 
 (defn init-editor [compiler]
-  (as-> (editor/create "code-cljs" config-editor) $
-    (handle-events $
-      {:idle-msec 3000
-       :on-should-eval #(process-input compiler (editor/get-value $))})))
+  (let [my-editor (editor/create "code-cljs" config-editor)]
+    (handle-events my-editor
+                   {:idle-msec 3000
+                    :on-should-eval #(process-input compiler (editor/get-value my-editor))})))
 
 (defui Cljs-editor
   

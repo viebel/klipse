97b406481757f89a1b4d914ddea4c1a9f8b12133
Yehonathan Sharvit <viebel@gmail.com>
2016-06-18 23:14:50 +0300

    plugin: re-organize mode opts

--- src/klipse/plugin.cljs
+++ src/klipse/plugin.cljs
@@ -2,6 +2,8 @@
   (:require-macros
     [cljs.core.async.macros :refer [go]])
   (:require 
+    [cljs.spec :as s]
+    [goog.dom :refer [isElement]]
     [klipse.utils :refer [gist-path-page read-input-from-gist]]
     [cljs.core.async :refer [<!]]
     [clojure.string :as string :refer [trim split]]
@@ -9,32 +11,28 @@
     [klipse.ui.editors.common :refer [handle-events]]
     [klipse.ui.editors.editor :refer [create-editor-after-element replace-element-by-editor set-value get-value]]
     [gadjett.core :as gadjett :refer-macros [dbg]]
-    #_[klipse.compiler :refer [str-eval-async str-eval str-compile-async]]
+    [klipse.compiler :refer [str-eval-async str-eval str-compile-async]]
     [klipse.js-eval :refer [str-eval-js-async]]))
 
 (def app-url "http://app.klipse.tech")
+(def selector->mode
+  {"selector" "eval-clojure"
+   "selector_js" "transpile-clojurescript"
+   "selector_eval_js" "eval-javascript"})
 
-(def language->eval-fn 
-  {;::eval-clojure str-eval-async
-   ::eval-javascript str-eval-js-async
-   ;::transpile-javascript str-compile-async
-   })
-
-(def language->editor-in-mode
-  {::eval-clojure "clojure"
-   ::eval-javascript "javascript"
-   ::transpile-javascript "clojure"})
-
-(def language->editor-out-mode
-  {::eval-clojure "clojure"
-   ::eval-javascript "javascript"
-   ::transpile-javascript "javascript"})
-
-(def language->comment-str
-  {::eval-clojure ";"
-   ::eval-javascript "//"
-   ::transpile-javascript ";"}
-  )
+(def mode-options {
+  "eval-clojure" {:editor-in-mode "clojure"
+                  :editor-out-mode "clojure"
+                  :eval-fn str-eval-async
+                  :comment-str ";"}
+  "transpile-clojurescript" {:editor-in-mode "clojure"
+                  :editor-out-mode "javascript"
+                  :eval-fn str-compile-async
+                  :comment-str ";"}
+  "eval-javascript" {:editor-in-mode "javascript"
+                  :editor-out-mode "javascript"
+                  :eval-fn str-eval-js-async
+                  :comment-str "//"}})
 
 (def editor-options
   {:matchBrackets true 
@@ -57,37 +55,32 @@
     (->> (split s ",")
          (map trim))))
 
-(defn prepend-comment [body txt language]
-  (let [comment-str (language->comment-str language)]
-    (str comment-str txt "\n" body)))
-
-(defn content-from-gist [element language]
+(defn content-from-gist [element comment-str]
   (go
     (when-let [gist-id (or
                          (-> (aget element "dataset")
                              (aget "gistId")) nil)]
       (let [gist-content (<! (read-input-from-gist gist-id))
             gist-intro (str "loaded from gist: " (gist-path-page gist-id))]
-        (prepend-comment gist-content gist-intro language)))))
+        (str comment-str gist-intro "\n" gist-content)))))
 
-(defn content [element language]
+(defn content [element comment-str]
   (go
     (or
-      (<! (content-from-gist element language))
+      (<! (content-from-gist element comment-str))
       (aget element "textContent");goog.dom/getTextContent removes new lines
       )))
 
-(defn ^:export klipsify [element language]
+(defn klipsify-with-opts [element {:keys [editor-in-mode editor-out-mode eval-fn comment-str]}]
   (go
     (when element
       (let [my-dataset (aget element "dataset")
             static-fns (read-string-or-val (aget my-dataset "staticFns") false)
-            external-libs (dbg (string->array (or (dbg (aget my-dataset "externalLibs")) nil)))
-            eval-fn (language->eval-fn language)
+            external-libs (dbg (string->array (or (aget my-dataset "externalLibs") nil)))
             eval-fn-with-args #(eval-fn % (dbg {:static-fns static-fns :external-libs external-libs}))
-            in-editor-options (assoc editor-options :mode (language language->editor-in-mode))
-            out-editor-options (assoc editor-options :mode (language language->editor-out-mode) :readOnly true)
-            clj-in (dbg (<! (content element language)))
+            in-editor-options (assoc editor-options :mode editor-in-mode)
+            out-editor-options (assoc editor-options :mode editor-out-mode :readOnly true)
+            clj-in (dbg (<! (content element comment-str)))
             out-editor (create-editor-after-element element ";the evaluation will appear here (soon)..." out-editor-options); must be called before `element` is replaced
             in-editor (replace-element-by-editor element clj-in in-editor-options)]
         (set-value out-editor (dbg (<! (eval-fn-with-args clj-in))))
@@ -96,22 +89,38 @@
                         :base-url app-url
                         :on-should-eval #(eval-in-editor eval-fn-with-args out-editor in-editor)})))))
 
+(s/def ::dom-element isElement)
+(s/def ::editor-in-mode string?)
+(s/def ::editor-out-mode string?)
+(s/def ::eval-fn fn?)
+(s/def ::comment-str string?)
 
-(defn ^:export klipsify-elements [elements language]
-  (print "klipsify-elements: " elements language)
-  (dbg language)
+(s/def ::options (s/keys :req-un [::editor-in-mode ::editor-out-mode ::eval-fn ::comment-str])) 
+
+(s/fdef klipsify-with-opts 
+        :args (s/cat :element ::dom-element
+                     :opts ::options))
+
+(s/instrument #'klipsify-with-opts)
+(defn ^:export klipsify [element mode]
+  (if-let [opts (mode-options mode)]
+    (klipsify-with-opts element opts)
+    (go (print "cannot find options for mode: " mode ". Supported modes: " (keys mode-options)))))
+
+(defn ^:export klipsify-elements [elements mode]
+  (print "klipsify-elements: " elements mode)
   (go
     (time
       (doseq [element elements]
         (print "klipsify-elements: " element)
-        (<! (klipsify element language))))))
+        (<! (klipsify element mode))))))
 
-(defn ^:export init-clj [{:keys [selector selector_js selector_eval_js] :as settings}]
+(defn ^:export init-clj [settings]
   (dbg settings)
-  (klipsify-elements (dbg (array-seq (js/document.querySelectorAll selector_eval_js))) ::eval-javascript)
-  (klipsify-elements (dbg (array-seq (js/document.querySelectorAll selector_js))) ::transpile-javascript)
-  (klipsify-elements (dbg (array-seq (js/document.querySelectorAll selector))) ::eval-clojure))
+  (doseq [selector-name (dbg (keys selector->mode))]
+    (when-let [selector (settings selector-name)]
+      (klipsify-elements (dbg (array-seq (js/document.querySelectorAll selector))) (selector->mode selector-name)))))
 
 (defn ^:export init [js-settings]
-  (init-clj (js->clj js-settings :keywordize-keys true)))
+  (init-clj (js->clj js-settings :keywordize-keys false)))
 

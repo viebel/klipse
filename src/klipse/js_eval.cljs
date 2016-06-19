ac306aa710c4c7c1e1601706d1ddd29865e2266b
Yehonathan Sharvit <viebel@gmail.com>
2016-06-19 22:46:22 +0300

    js: eval in the global scope

--- src/klipse/js_eval.cljs
+++ src/klipse/js_eval.cljs
@@ -8,12 +8,13 @@
     [klipse.plugin :refer [register-mode]]
     [gadjett.core :as gadjett :refer-macros [dbg]]))
 
-(def known-external-lib
+(def known-external-libs
   {
    "immutable" "https://raw.githubusercontent.com/facebook/immutable-js/master/dist/immutable.min.js"
    "jQuery" "https://code.jquery.com/jquery-2.2.4.min.js"
    "underscore" "http://underscorejs.org/underscore-min.js"})
 
+(def eval-in-global-scope js/eval); this is the trick to make `eval` work in the global scope: http://perfectionkills.com/global-eval-what-are-the-options/
 (defn load-scripts [scripts]
   (go-loop [the-scripts scripts]
            (if (seq the-scripts)
@@ -23,23 +24,23 @@
                (if (= 200 status)
                  (do
                    (println "evaluating:" script)
-                   (js/eval body)
+                   (eval-in-global-scope body)
                    (recur (rest the-scripts)))
                  [:error status script]))
              [:ok])))
 
 (defn external-lib-path [lib-name-or-url]
-  (get known-external-lib lib-name-or-url lib-name-or-url))
+  (get known-external-libs lib-name-or-url lib-name-or-url))
 
 (defn str-eval-js-async [exp {:keys [external-libs] :or {external-libs nil}}]
   (go
     (let [[status http-status script] (<! (load-scripts (map external-lib-path external-libs)))]
       (if (= :ok status)
         (dbg (try (-> exp
-                 js/eval
-                 str)
-              (catch js/Object o
-                (str o))))
+                      eval-in-global-scope
+                      str)
+                  (catch js/Object o
+                    (str o))))
         (str "Cannot load script: " script "\n"
              "error: " http-status)))))
 

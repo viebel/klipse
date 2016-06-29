995c481b7e0854f4201ee665a65d94587343c589
Yehonathan Sharvit <viebel@gmail.com>
2016-06-30 01:11:57 +0300

    bump version + add missing file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klipse "1.9.0"
+(defproject klipse "1.10.0"
   :description "Cljs compiler in cljs"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.93"]

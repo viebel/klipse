86093553885e80b280fd060ca05b7fd696d293b7
Yehonathan Sharvit <viebel@gmail.com>
2016-10-24 22:05:54 +0200

    version 3.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klipse "3.2.1"
+(defproject klipse "3.3.0"
   :description "Cljs compiler in cljs"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]

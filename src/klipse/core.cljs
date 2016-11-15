e039dd9a540eace541cfd375fd33f93643afbe23
Yehonathan Sharvit <viebel@gmail.com>
2016-11-15 17:50:32 +0000

    bump version 3.8.0

--- src/klipse/core.cljs
+++ src/klipse/core.cljs
@@ -5,7 +5,7 @@
 (enable-console-print!)
 (gadjett/settings! :max-function-calls 100)
 
-(def version "3.7.0")
+(def version "3.8.0")
 (js/console.info "Klipse " version)
 (js/console.info "Clojurescript " *clojurescript-version*)
 

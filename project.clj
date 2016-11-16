62d8ea1244c8fc482f45212bd65661b557cf0ba5
Yehonathan Sharvit <viebel@gmail.com>
2016-11-16 07:35:33 +0000

    update README etc..

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klipse "3.8.1"
+(defproject klipse "3.9.0"
   :description "Cljs compiler in cljs"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]

34c30e6e65757a48c0a0e9742c0281834e41af31
Yehonathan Sharvit <viebel@gmail.com>
2016-12-12 08:05:34 +0200

    transpile jsx

--- src/klipse/lang/jsx.cljs
+++ src/klipse/lang/jsx.cljs
@@ -1,23 +1,19 @@
 (ns klipse.lang.jsx
   (:require-macros
     [gadjett.core :refer [dbg]]
-    [purnam.core :refer [? ! !>]]
-    [cljs.core.async.macros :refer [go go-loop]])
+    [purnam.core :refer [!>]]
+    [cljs.core.async.macros :refer [go]])
   (:require
-    [klipse.utils :refer [load-scripts]]
-    [cljs-http.client :as http]
-    [clojure.string :as string]
-    [cljs.core.async :refer [<! chan put!]]
     [klipse.common.registry :refer [register-mode]]))
 
 (def eval-in-global-scope js/eval); this is the trick to make `eval` work in the global scope: http://perfectionkills.com/global-eval-what-are-the-options/
 
 
 (defn babel [src]
-   (dbg (-> (!> js/Babel.transform src #js {:presets #js ["react" "es2015"]})
-       (aget "code"))))
+   (-> (!> js/Babel.transform src #js {:presets #js ["react"]})
+       (aget "code")))
 
-(defn str-eval-js-async [exp {:keys [external-libs] :or {external-libs nil}}]
+(defn eval-jsx [exp _]
   (go
     (try
       (-> exp
@@ -27,11 +23,24 @@
       (catch :default o
         (str o)))))
 
+(defn transpile-jsx [exp _]
+  (go
+    (try
+      (babel exp)
+      (catch :default o
+        (str o)))))
 
-(def opts {:editor-in-mode "text/jsx"
+(def eval-opts {:editor-in-mode "text/jsx"
            :editor-out-mode "javascript"
-           :eval-fn str-eval-js-async
+           :eval-fn eval-jsx
            :external-scripts ["https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.21.0/mode/jsx/jsx.min.js" "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.1/babel.min.js"]
            :comment-str "//"})
 
-(register-mode "eval-jsx" "selector_jsx" opts)
+(def transpile-opts {:editor-in-mode "text/jsx"
+           :editor-out-mode "javascript"
+           :eval-fn transpile-jsx
+           :external-scripts ["https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.21.0/mode/jsx/jsx.min.js"]
+           :comment-str "//"})
+
+(register-mode "eval-jsx" "selector_jsx" eval-opts)
+(register-mode "transpile-jsx" "selector_transpile_jsx" transpile-opts)

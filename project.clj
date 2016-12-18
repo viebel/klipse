1e68289515241181765f86bd66a139a14e5cd771
Yehonathan Sharvit <viebel@gmail.com>
2016-12-18 19:48:33 +0200

    version 4.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klipse "4.3.1"
+(defproject klipse "4.4.0"
   :description "Cljs compiler in cljs"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]

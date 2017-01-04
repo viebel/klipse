872ad66002368f05f01a7b0f96b8fa0a80cb6b65
Yehonathan Sharvit <viebel@gmail.com>
2017-01-04 13:09:53 +0200

    version 5.2.0

--- src/klipse/core.cljs
+++ src/klipse/core.cljs
@@ -5,7 +5,7 @@
 (enable-console-print!)
 (gadjett/settings! :max-function-calls 100)
 
-(def version "5.1.0")
+(def version "5.2.0")
 (js/console.info "Klipse " version)
 (js/console.info "Clojurescript " *clojurescript-version*)
 

4a02c690703658b4a1f43983f5a728f7c6ba0e56
Yehonathan Sharvit <viebel@gmail.com>
2017-01-07 20:48:24 +0200

    it works

--- src/klipse/lang/reagent.cljs
+++ src/klipse/lang/reagent.cljs
@@ -2,21 +2,26 @@
   (:require-macros
    [gadjett.core :as gadjett :refer [dbg]])
   (:require
+   [goog.dom :as gdom]
    [cljs.reader :refer [read-string]]
    [clojure.string :as string]
-   [cljs.core.async :refer [chan]]
+   [cljs.core.async :refer [chan close!]]
    [klipse.common.registry :refer [codemirror-mode-src register-mode]]
    [klipse.lang.clojure :refer [str-eval-async split-expressions]]))
 
 
-(defn eval-reagent [src {:keys [container-id] :as opts}]
-  ;; TODO - Jan 4 2016 - handle exceptions
-  ;; It's tricky because of `:no-result true`
-  (let [exps (split-expressions src)
-        component (last exps)
-        other-code (string/join "\n" (drop-last 1 exps))
-        code (str other-code `(reagent.core/render-component  ~(read-string component) (js/document.getElementById ~container-id)))]
-    (str-eval-async code opts)))
+(defn eval-reagent [src {:keys [container-id container] :as opts}]
+  (try
+    (let [exps (split-expressions src)
+          component (last exps)
+          other-code (string/join "\n" (drop-last 1 exps))
+          code (str other-code `(reagent.core/render-component  ~(read-string component) (js/document.getElementById ~container-id)))]
+      (str-eval-async code opts))
+    (catch :default e
+      (gdom/setTextContent container (str e))
+      (let [c (chan)]
+        (close! c)
+        c))))
 
 (def opts {:editor-in-mode "clojure"
            :editor-out-mode "clojure"

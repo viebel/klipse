90044e86dc6cf6cea908ac0912291d95edfbc86f
Yehonathan Sharvit <viebel@gmail.com>
2017-01-07 20:00:32 +0200

    wip

--- src/klipse/lang/reagent.cljs
+++ src/klipse/lang/reagent.cljs
@@ -3,15 +3,19 @@
    [gadjett.core :as gadjett :refer [dbg]])
   (:require
    [cljs.reader :refer [read-string]]
+   [clojure.string :as string]
    [cljs.core.async :refer [chan]]
    [klipse.common.registry :refer [codemirror-mode-src register-mode]]
-   [klipse.lang.clojure :refer [str-eval-async]]))
+   [klipse.lang.clojure :refer [str-eval-async split-expressions]]))
 
 
 (defn eval-reagent [src {:keys [container-id] :as opts}]
   ;; TODO - Jan 4 2016 - handle exceptions
   ;; It's tricky because of `:no-result true`
-  (let [code (str `(reagent.core/render-component  ~(read-string src) (js/document.getElementById ~container-id)))]
+  (let [exps (split-expressions src)
+        component (last exps)
+        other-code (string/join "\n" (drop-last 1 exps))
+        code (str other-code `(reagent.core/render-component  ~(read-string component) (js/document.getElementById ~container-id)))]
     (str-eval-async code opts)))
 
 (def opts {:editor-in-mode "clojure"

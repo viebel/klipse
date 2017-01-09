65d8f0686a9b388eefbac5cc1ca9fdc44505b32b
Yehonathan Sharvit <viebel@gmail.com>
2017-01-09 23:44:38 +0200

    codemirror root is configurable

--- src/klipse/common/registry.cljs
+++ src/klipse/common/registry.cljs
@@ -1,12 +1,15 @@
 (ns klipse.common.registry
+  (:require
+   [klipse.utils :refer [klipse-settings]])
   (:require-macros
-    [gadjett.core :refer [dbg]]))
+   [gadjett.core :refer [dbg]]))
 
 (def selector->mode (atom {}))
 (def mode-options (atom {}))
 
 (defn codemirror-mode-src [mode]
-  (str "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.21.0/mode/" mode "/" mode ".min.js"))
+  (let [root (:codemirror_root (klipse-settings) "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.21.0/mode")]
+    (str root "/" mode "/" mode ".min.js")))
 
 (defn register-mode [mode selector opts]
   (js/console.info "register-mode: " mode selector)

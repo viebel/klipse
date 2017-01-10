b2f4ad444d8d9c740deaba87b12fe0d14d5cc604
Yehonathan Sharvit <viebel@gmail.com>
2017-01-10 15:06:13 +0200

    set a var for the container

--- src/klipse/ui/outputs/container.cljs
+++ src/klipse/ui/outputs/container.cljs
@@ -6,6 +6,8 @@
 
 (defui Container
   Object
+  (componentDidMount [this]
+    (set! js/klipse-container (js/document.getElementById "klipse-container")))
   (render [this]
           (dom/div #js {:id "klipse-container-wrapper"}
                    (dom/div #js {:id "klipse-container"}
@@ -14,10 +16,11 @@
                                    ".")
                             (dom/p nil  "You can access it with "
                                    (dom/code nil "(js/document.getElementById \"klipse-container\")")
+                                   " or with " (dom/code nil "js/klipse-container") 
                                    ".")
                             (dom/p nil "For instance, try to copy and paste the following code into the top left box and press 'Ctrl-Enter` or wait for 3 seconds:")
                             (dom/pre nil
                                      (dom/code nil
-                                               "(set!\n (.-innerHTML (js/document.getElementById \"klipse-container\"))\n \"<div style='color:blue;'> Hello <b>World</b>!</div>\")"))))))
+                                               "(set!\n (.-innerHTML js/klipse-container)\n \"<div style='color:blue;'> Hello <b>World</b>!</div>\")"))))))
 
 (def container (om/factory Container))

229724c8ea28cdc8549d3f82425bfc56a4693a2d
Yehonathan Sharvit <viebel@gmail.com>
2017-01-12 23:06:16 +0200

    version 6.1.1 - html mode

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klipse "6.1.0"
+(defproject klipse "6.1.1"
   :description "Embeddable multi-language WEB REPL"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]
--- src/klipse/lang/html.cljs
+++ src/klipse/lang/html.cljs
@@ -12,6 +12,7 @@
 
 (def opts {:editor-in-mode "text/html"
            :editor-out-mode "text"
+           :default-editor "html"
            :external-scripts [(codemirror-mode-src "xml") (codemirror-mode-src "htmlmixed")]
            :eval-fn my-eval
            :comment-str "; "})

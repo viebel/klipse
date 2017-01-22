0535551305e43f3fff635a2ddb83c0802cc17cd6
Yehonathan Sharvit <viebel@gmail.com>
2017-01-22 09:28:20 +0200

    6.5.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klipse "6.5.0"
+(defproject klipse "6.5.1"
   :description "Embeddable multi-language WEB REPL"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]

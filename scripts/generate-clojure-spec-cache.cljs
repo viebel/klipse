0eed0c26f4ff84b913b8dd3179dd018b99f33075
Yehonathan Sharvit <viebel@gmail.com>
2017-03-11 19:49:53 +0200

    analysis cache generation: include clojure namespaces

--- scripts/generate-clojure-spec-cache.cljs
+++ scripts/generate-clojure-spec-cache.cljs
@@ -2,6 +2,6 @@
   (:require [clojure.string :as string]))
 
 (let [nexeres (js/require "nexeres")]
-  (doseq [resource (filter #(re-matches #"cljs.*" %) (.keys nexeres))]
+  (doseq [resource (filter #(re-matches #"cljs/.*|clojure/.*" %) (.keys nexeres))]
     (let [filename (str "docs/cache-cljs/" (string/replace resource #"[/\\]" "_SLASH_"))]
       (js/fs.writeFile filename (js/zlib.inflateSync (.get nexeres resource)) #(println "Done: " filename)))))

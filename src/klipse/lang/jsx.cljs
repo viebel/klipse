304106457f303e6cdd595d5a1d47db952ff01908
Yehonathan Sharvit <viebel@gmail.com>
2017-03-18 22:50:22 +0200

    Render jsx (#250)
    * render jsx
    
    * render react

--- src/klipse/lang/jsx.cljs
+++ src/klipse/lang/jsx.cljs
@@ -1,15 +1,16 @@
 (ns klipse.lang.jsx
   (:require-macros
-    [gadjett.core :refer [dbg]]
-    [purnam.core :refer [!>]]
-    [cljs.core.async.macros :refer [go]])
+   [gadjett.core :refer [dbg]]
+   [purnam.core :refer [!>]]
+   [cljs.core.async.macros :refer [go]])
   (:require
-    [klipse.utils :refer [eval-in-global-scope]]    
-    [klipse.common.registry :refer [codemirror-mode-src register-mode scripts-src]]))
+   [goog.dom :as gdom]
+   [klipse.utils :refer [eval-in-global-scope verbose?]]    
+   [klipse.common.registry :refer [codemirror-mode-src register-mode scripts-src]]))
 
 (defn babel [src]
-   (-> (!> js/Babel.transform src #js {:presets #js ["react"]})
-       (aget "code")))
+  (-> (!> js/Babel.transform src #js {:presets #js ["react"]})
+      (aget "code")))
 
 (defn eval-jsx [exp _]
   (go
@@ -20,6 +21,35 @@
       "//Evaluation done"
       (catch :default o
         (str o)))))
+
+(defn render-jsx* [exp {:keys [container-id]}]
+  (try
+    (babel exp)
+    (let [wrapped-exp (str "ReactDOM.render(" exp ", document.getElementById('" container-id "'))")]
+      (when (verbose?)
+        (js/console.info "render-jsx - wrapped expression:" wrapped-exp))
+      (-> (babel wrapped-exp)
+          eval-in-global-scope))
+    (catch :default e
+      (let [container (js/document.getElementById container-id)]
+        (gdom/setTextContent container (str e))))))
+
+(defn render-jsx [exp opts]
+  (go (render-jsx* exp opts)))
+
+(defn render-react* [exp {:keys [container-id]}]
+  (try
+    (let [wrapped-exp (str "ReactDOM.render(" exp ", document.getElementById('" container-id "'))")]
+      (when (verbose?)
+        (js/console.info "render-react - wrapped expression:" wrapped-exp))
+      (-> wrapped-exp
+          eval-in-global-scope))
+    (catch :default e
+      (let [container (js/document.getElementById container-id)]
+        (gdom/setTextContent container (str e))))))
+
+(defn render-react [exp opts]
+  (go (render-react* exp opts)))
 
 (defn transpile-jsx [exp _]
   (go
@@ -40,5 +70,22 @@
                      :external-scripts [(codemirror-mode-src "xml") (codemirror-mode-src "javascript") (codemirror-mode-src "jsx") (scripts-src "babel.min.js")]
                      :comment-str "//"})
 
+
+(def render-opts {:editor-in-mode "text/jsx"
+                  :editor-out-mode "javascript"
+                  :eval-fn render-jsx
+                  :no-result true
+                  :external-scripts [(codemirror-mode-src "xml") (codemirror-mode-src "javascript") (codemirror-mode-src "jsx") (scripts-src "babel.min.js")]
+                  :comment-str "//"})
+
+(def render-react-opts {:editor-in-mode "javascript"
+                        :editor-out-mode "javascript"
+                        :eval-fn render-react
+                        :no-result true
+                        :external-scripts [ (codemirror-mode-src "javascript")]
+                        :comment-str "//"})
+
 (register-mode "eval-jsx" "selector_jsx" eval-opts)
 (register-mode "transpile-jsx" "selector_transpile_jsx" transpile-opts)
+(register-mode "render-jsx" "selector_render_jsx" render-opts)
+(register-mode "render-react" "selector_react" render-react-opts)

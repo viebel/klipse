99d6804103108b08c21b63f3c16b3a0e7cd3137a
Yehonathan Sharvit <viebel@gmail.com>
2018-12-18 23:32:59 +0200

    7.7.0 - upgrade cljs

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
 (defproject klipse "7.7.0"
   :description "Embeddable multi-language WEB REPL"
   :resource-paths ["scripts" "src" "resources" "target"]
-  :clean-targets ^{:protect false} ["resources/public/plugin_prod/js"
-                                    "resources/public/plugin/js"]
   :min-lein-version "2.8.1"
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
-  :plugins [[lein-cljsbuild "1.1.5"]
+  :plugins [[lein-cljsbuild "1.1.7"]
             [lein-hiera "0.9.5"]
             [lein-tools-deps "0.4.1"]]
   :hiera {:path "deps-graph.png"

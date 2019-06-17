523e8066ffa4c4e41165ed8b24cd48e75c7a6755
Yehonathan Sharvit <viebel@gmail.com>
2019-06-17 08:58:55 +0300

    7.9.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject klipse "7.8.1"
+(defproject klipse "7.9.0"
   :description "Embeddable multi-language WEB REPL"
   :resource-paths ["scripts" "src" "resources" "target"]
   :min-lein-version "2.8.1"

c3502de9368cb161005990039c1034d4bddf61d8
Yehonathan Sharvit <viebel@gmail.com>
2020-08-15 22:06:16 +0300

    cut version 7.10.4

--- src/klipse/core.cljs
+++ src/klipse/core.cljs
@@ -5,7 +5,7 @@
 (enable-console-print!)
 (gadjett/settings! :max-function-calls 100)
 
-(def version "7.10.3")
+(def version "7.10.4")
 (js/console.info "Klipse " version)
 (js/console.info "Clojurescript "  *clojurescript-version*)
 

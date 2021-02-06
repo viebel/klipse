ae37500bc3ea8458941c112f2869e4f607107ce2
Yehonathan Sharvit <viebel@gmail.com>
2021-02-07 01:18:36 +0200

    Python: Support variable sharing between snippets

--- src/klipse/lang/python.cljs
+++ src/klipse/lang/python.cljs
@@ -21,25 +21,26 @@
 (defn str-eval-async [exp {:keys [container-id]}]
   (init)
   (let [c (chan)]
-    (j/call js/Sk :configure #js {:output #(put! c %)
-                             :read builtin-read })
-    (-> 
+    (j/call js/Sk :configure #js {:output        #(put! c %)
+                                  :read          builtin-read
+                                  :retainglobals true})
+    (->
       (j/call-in js/Sk [:misceval :asyncToPromise]
-       (fn []
-            (put! c "Output:\n")
-            (j/assoc-in! js/Sk [:TurtleGraphics :target] container-id)
-            (j/call js/Sk :importMainWithBody "<stdin>" false exp true)))
+                 (fn []
+                   (put! c "Output:\n")
+                   (j/assoc-in! js/Sk [:TurtleGraphics :target] container-id)
+                   (j/call js/Sk :importMainWithBody "<stdin>" false exp true)))
       (.then (fn [mod]
                (j/call js/console :info "success to eval skulpt: "))
              (fn [err]
                (put! c (str "error: " err)))))
     c))
 
-(def opts {:editor-in-mode "python"
-           :editor-out-mode "python"
-           :eval-fn str-eval-async
-           :beautify? false
-           :external-scripts [(codemirror-mode-src "python") (scripts-src "skulpt.min.js") (scripts-src "skulpt-stdlib.js")]
-           :comment-str "#"})
+(def opts {:editor-in-mode   "python"
+           :editor-out-mode  "python"
+           :eval-fn          str-eval-async
+           :beautify?        false
+           :external-scripts [(codemirror-mode-src "python") (scripts-src "skulpt.0.10.0.min.js") (scripts-src "skulpt-stdlib.0.10.0.js")]
+           :comment-str      "#"})
 
 (register-mode "eval-python-client" "selector_eval_python_client" opts)

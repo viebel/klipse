5ce8867e79436f17a0e5db4647487ebe5c6bf53b
Yehonathan Sharvit <viebel@gmail.com>
2021-08-29 10:59:31 +0300

    Cut version 7.11.1

--- src/klipse/lang/golang.cljs
+++ src/klipse/lang/golang.cljs
@@ -1,50 +1,56 @@
 (ns klipse.lang.golang
   (:require-macros
-   [gadjett.core :as gadjett :refer [dbg]]
    [cljs.core.async.macros :refer [go]])
   (:require
-   [goog.dom :as gdom]
+   [clojure.string :refer [blank?]]
    [klipse.utils :refer [add-script-tag! verbose?]]
-   [clojure.string :as string]
-   [cljs.core.async :refer [chan close! <! put!]]
+   [cljs.core.async :refer [chan <! put!]]
    [klipse.common.registry :refer [codemirror-mode-src register-mode wasm-src scripts-src scripts-src]]
    [applied-science.js-interop :as j]))
 
 (defonce ^:dynamic *loaded* false)
 
-(defn load-module! [ready-chan]
-  (let [xhr (new js/window.XMLHttpRequest)]
+;; Dirty trick to prevent google closure compiler to use `fs` global variable
+;; as it is going to be set by "wasm_exec.js"
+(j/assoc! js/window "fs" nil)
+
+(defn load-module! []
+  (let [c   (chan)
+        xhr (new js/window.XMLHttpRequest)]
     (j/call xhr :open "GET" (wasm-src "yaegi.wasm"))
     (j/assoc! xhr :responseType "arraybuffer")
     (j/call xhr :overrideMimeType "application/javascript")
-    (j/assoc! xhr :onload (fn []
-                            (when (verbose?)
-                              (js/console.log "wasm loaded:"))
-                            (let [gos (new js/Go)]
-                              (.then (j/call window.WebAssembly :instantiate (aget xhr "response") (aget gos "importObject")) (fn [result]
-                                                                                                                                (j/call gos :run (aget result "instance"))
-                                                                                                                                (put! ready-chan [:ok])
-                                                                                                                                ))
-                              )
-                            ))
-    (j/call xhr :send nil)))
+    (j/assoc! xhr :onload
+              (fn []
+                (when (verbose?)
+                  (js/console.log "wasm loaded:"))
+                (let [gos (new js/Go)]
+                  (.then
+                    (j/call js/WebAssembly :instantiate (aget xhr "response") (aget gos "importObject"))
+                    (fn [result]
+                      (j/call gos :run (aget result "instance"))
+                      (put! c [:ok]))))))
+    (j/call xhr :send nil)
+    c))
 
 (defn the-eval [src _]
-  (go (let [ready-chan (chan)]
-        (when-not *loaded*
-          (let [script-chan (add-script-tag! (scripts-src "wasm_exec.js"))]
-            (<! script-chan))
-          (load-module! ready-chan)
-          (<! ready-chan)
-          (set! *loaded* true)))
-      (let [result (j/call js/window :evalGo src)]
-        (str "stdout:\n" (aget result "stdout") "\nstderr:\n" (aget result "stderr")))))
+  (go
+    (when-not *loaded*
+      (<! (add-script-tag! (scripts-src "wasm_exec.js")))
+      (<! (load-module!))
+      (set! *loaded* true))
+    (let [result (j/call js/window :evalGo src)]
+      (if (blank? (aget result "error"))
+        (if (aget result "stderr")
+          (str (aget result "stdout"))
+          (str "stdout:\n" (aget result "stdout") "\nstderr:\n" (aget result "stderr")))
+        (str "error:\n" (aget result "error"))))))
 
 
-(def opts {:editor-in-mode "text/x-go"
-           :editor-out-mode "text"
-           :eval-fn the-eval
-           :external-scripts [(codemirror-mode-src "go") ]
-           :comment-str "--"})
+(def opts {:editor-in-mode   "text/x-go"
+           :editor-out-mode  "text"
+           :eval-fn          the-eval
+           :external-scripts [(codemirror-mode-src "go")]
+           :comment-str      "--"})
 
-(register-mode   "golang" "selector_golang" opts)
+(register-mode "golang" "selector_golang" opts)

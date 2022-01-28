3a091a9db9bcc078310a3e3c2cfa446597ecdbe1
Yehonathan Sharvit <viebel@gmail.com>
2022-01-28 07:59:41 +0200

    Bump version

--- src/klipse/core.cljs
+++ src/klipse/core.cljs
@@ -5,7 +5,7 @@
 (enable-console-print!)
 (gadjett/settings! :max-function-calls 100)
 
-(def version "7.11.2")
+(def version "7.11.3")
 (js/console.info "Klipse " version)
 (js/console.info "Clojurescript "  *clojurescript-version*)
 

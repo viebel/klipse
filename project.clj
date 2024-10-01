752c84002bfc2d3226599c997777ed4d157e69e1
Tim Hammerquist <tim@penryu.dev>
2024-09-30 22:03:57 -0700

    chore: bump clj, cljs, core.async and a few other lips

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject viebel/klipse "7.11.4"
+(defproject viebel/klipse "7.11.5"
   :description "Embeddable multi-language WEB REPL"
   :license "GPL-3.0"
   :url "https://github.com/viebel/klipse"
@@ -8,8 +8,8 @@
   :lein-tools-deps/config {:config-files [:install :user :project]}
   :profiles {:deploy {:resource-paths ^:replace ["src"]
                       :lein-tools-deps/config ^:replace {:config-files [:install :user]}}}
-  :deploy-repositories [["clojars"  {:sign-releases false
-                                     :url "https://clojars.org/repo"}]]
+  :deploy-repositories [["clojars" {:sign-releases false
+                                    :url "https://clojars.org/repo"}]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-hiera "0.9.5"]
             [lein-tools-deps "0.4.1"]]
@@ -20,8 +20,7 @@
           :cluster-depth 2
           :trim-ns-prefix true}
   :cljsbuild {:builds {:plugin {:source-paths ["src/klipse/run/plugin"]
-                                :compiler {
-                                           :output-to "resources/public/plugin/js/klipse_plugin.js"
+                                :compiler {:output-to "resources/public/plugin/js/klipse_plugin.js"
                                            :output-dir "resources/public/plugin/js"
                                            :pretty-print false
                                            :optimize-constants true
@@ -29,20 +28,18 @@
                                            ;:elide-asserts true
                                            :closure-defines {klipse.core/version
                                                              ~(->> (slurp "project.clj")
-                                                                (re-seq #"\".*\"")
-                                                                (first))}
+                                                                   (re-seq #"\".*\"")
+                                                                   (first))}
                                            :optimizations :simple
                                            :verbose false}}
-                       :plugin-prod {
-                                     :source-paths ["src/klipse/run/plugin_prod"]
-                                     :compiler {
-                                                :output-to "resources/public/plugin_prod/js/klipse_plugin.min.js"
+                       :plugin-prod {:source-paths ["src/klipse/run/plugin_prod"]
+                                     :compiler {:output-to "resources/public/plugin_prod/js/klipse_plugin.min.js"
                                                 :output-dir "resources/public/plugin_prod/js"
                                                 :pretty-print true
                                                 :elide-asserts false
                                                 :optimizations :advanced
                                                 :closure-defines {klipse.core/version
                                                                   ~(->> (slurp "project.clj")
-                                                                     (re-seq #"\".*\"")
-                                                                     (first))}
+                                                                        (re-seq #"\".*\"")
+                                                                        (first))}
                                                 :verbose true}}}})
